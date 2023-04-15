b79fff659dcdacef051b38de8bf8afc414b6f74d
PHAM VO HOANG PHI <phiphamvohoang@gmail.com>
2023-04-15 14:18:55 +0700

    upload Cart

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -1,3 +1,4 @@
+//gắn links footer vào các layout khác
 const footerContainer = document.querySelector('#footer');
       const footerRequest = new XMLHttpRequest();
       footerRequest.open('GET', '/HTML/footer.html');
@@ -5,3 +6,11 @@ const footerContainer = document.querySelector('#footer');
         footerContainer.innerHTML = footerRequest.responseText;
       };
       footerRequest.send();
+//end
+
+// const OpenLinksFooter = document.querySelector('.down_title')
+// const LinksFooter = document.querySelector('.list_title')
+
+// OpenLinksFooter.addEventListener('click', function() {
+// 	LinksFooter.style.display = 'block';
+// });
